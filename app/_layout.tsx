178305720a678edf6d2ec79772a88adb174cf844
Megan Liu <meganliukent@gmail.com>
2026-04-09 16:39:20 -0700

    Wrap RootLayout in VitaminsProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,14 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { VitaminsProvider } from '../VitaminsContext';
 
 export default function RootLayout() {
   return (
-    <>
+    <VitaminsProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="dark" />
-    </>
+    </VitaminsProvider>
   );
 }
