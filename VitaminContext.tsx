d2219c2b5d507666d8dea6aba73822916622532f
Megan Liu <meganliukent@gmail.com>
2026-04-15 18:30:49 -0700

    Add pickedPet and pickedPlant to VitaminsContext

--- VitaminContext.tsx
+++ VitaminContext.tsx
@@ -8,20 +8,36 @@ export interface Supplement {
   withFood: boolean;
 }
 
-const VitaminsContext = createContext<{
+
+import { ImageSourcePropType } from 'react-native';
+
+interface VitaminsContextType {
   supplements: Supplement[];
   setSupplements: React.Dispatch<React.SetStateAction<Supplement[]>>;
-}>({
+  pickedPet: ImageSourcePropType | undefined;
+  setPickedPet: React.Dispatch<React.SetStateAction<ImageSourcePropType | undefined>>;
+  pickedPlant: ImageSourcePropType | undefined;
+  setPickedPlant: React.Dispatch<React.SetStateAction<ImageSourcePropType | undefined>>;
+}
+
+const VitaminsContext = createContext<VitaminsContextType>({
   supplements: [],
   setSupplements: () => {},
+  pickedPet: undefined,
+  setPickedPet: () => {},
+  pickedPlant: undefined,
+  setPickedPlant: () => {},
 });
+
 
 export const useVitamins = () => useContext(VitaminsContext);
 
 export const VitaminsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [supplements, setSupplements] = useState<Supplement[]>([]);
+  const [pickedPet, setPickedPet] = useState<ImageSourcePropType | undefined>(undefined);
+  const [pickedPlant, setPickedPlant] = useState<ImageSourcePropType | undefined>(undefined);
   return (
-    <VitaminsContext.Provider value={{ supplements, setSupplements }}>
+    <VitaminsContext.Provider value={{ supplements, setSupplements, pickedPet, setPickedPet, pickedPlant, setPickedPlant }}>
       {children}
     </VitaminsContext.Provider>
   );
