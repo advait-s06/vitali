9d33a674fbc9149fc920ef1bea272041a729c8ce
Advait Sankaran <advait.sankaran2006@gmail.com>
2026-04-17 08:45:33 -0700

    fixed page routing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,13 @@ import { VitaminsProvider } from '@/VitaminContext';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
+
 export default function RootLayout() {
+
   return (
     <VitaminsProvider>
       <Stack>
+        <Stack.Screen name="login"/>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="dark" />
